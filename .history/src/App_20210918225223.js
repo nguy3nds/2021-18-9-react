503ac7e669335f616c52a8c12fd2e750ef5583a7
Nguyễn Cảnh Nguyên <nguyencanhnguyen@MacBook-Pro-13-inch-M1-2020.local>
2021-09-18 22:53:11 +0700

    dsadasdasd

---
+++ .history/src/App_20210918225223.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul className="menu">
+            <li class="menu-item has-child">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="menu-item has-child">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="menu-item has-child">
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
