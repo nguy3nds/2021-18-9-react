3d994d5b6af5789560261b57c399caceed9198fe
Nguyễn Cảnh Nguyên <nguyencanhnguyen@MacBook-Pro-13-inch-M1-2020.local>
2021-09-19 06:51:17 +0700

    anyone commit

--- src/components/What.js
+++ src/components/What.js
@@ -4,6 +4,10 @@ export default function What() {
   return (
     <div>
       <h4>What is react router ?</h4>
+      <img
+        src="https://nordiccoder.com/app/uploads/2020/04/71-react-router.png"
+        alt=""
+      />
       <p>
         <b>React Router</b> is a standard library for routing in React.
       </p>
